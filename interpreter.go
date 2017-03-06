ebe4efa7618bd5a6a7048c4edff4b89c71ad2bc9
liuchibing <charlie428b@hotmail.com>
2017-03-06 16:29:14 +0800

    。。

--- interpreter.go
+++ interpreter.go
@@ -1,3 +1,6 @@
+/*
+	mathgo 包是一个数学工具--脚本解释器。语法类似 Scilab 。
+*/
 package mathgo
 
 type Interpreter interface {
--- ratInterpreter.go
+++ ratInterpreter.go
@@ -1,9 +1,15 @@
 package mathgo
 
+import "regexp"
+
+/*
+	实数运算解释器 -- 实现了Interpreter 接口
+ */
 type ratInterpreter struct {
-	vars map[string]Value
+	vars map[string]MthExp
 }
 
-func (ri *ratInterpeter) Run(exp string) bool {
-	
+func (ri *ratInterpreter) Run(exp string) bool {
+	regSetVar := regexp.MustCompile("(.+)=(.+);?")
+	regRunFunc := regexp.MustCompile("(.+)\\((.+)\\)")
 }
