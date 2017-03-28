1b096889826014f4a8668849982d37bac33b8cfc
liuchibing <charlie428b@hotmail.com>
2017-03-28 20:28:51 +0800

    继续

--- tokenizer.go
+++ tokenizer.go
@@ -1,14 +1,20 @@
 package mathgo
 
 import (
-	"errors"
-	"io"
 	"bufio"
+	"io"
 )
 
 type tokenizer struct {
-	input *bufio.Reader
+	input    *bufio.Reader
+	readMode int
 }
+
+const (
+	normal_readmode      = 0
+	string_readmode      = 1
+	escape_char_readmode = 2
+)
 
 func newTokenizer(reader *io.Reader) *tokenizer {
 	t := new(tokenizer)
@@ -18,15 +24,33 @@ func newTokenizer(reader *io.Reader) *tokenizer {
 }
 
 func (t *tokenizer) getNext() (string, error) {
-	var breakchar rune = nil
-	var buf string = ""
+	var buf string
 	for {
-		ch, err := t.input.ReadRune()
+		ch, _, err := t.input.ReadRune()
 		if err != nil {
 			return nil, err
 		}
 		switch ch {
-		case breakchar:
-			
+		case '+', '-':
+			if buf != "" {
+				t.input.UnreadRune()
+				return buf, nil
+			} else {
+				return string(ch), nil
+			}
+		case '"':
+			switch t.readMode {
+			case escape_char_readmode:
+				buf += string(ch)
+			case string_readmode:
+				buf += string(ch)
+				t.readMode = normal_readmode
+				return buf, nil
+			case normal_readmode:
+				t.readMode = string_readmode
+				buf += string(ch)
+			}
+		}
 	}
+
 }
